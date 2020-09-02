98214f4a197e13fe023676bf826012839dda081d
Ty Foster <tyfoster@outlook.com>
2020-09-02 00:56:33 -0700

    Add parse of file and write of sell prices

--- priceparser.py
+++ priceparser.py
@@ -1,6 +1,9 @@
 #imports
 import os
 
+sell_file = "worth.txt"
+buy_file = "server-price.txt"
+
 #class
 class PriceParser:
 
@@ -13,20 +16,42 @@ class PriceParser:
     #read input from txt file
     def read(self):
         # open file
+        ins = open(self.file, 'r')
+        # loop through lines
+        while True:
+            line = ins.readline()
+            # if at end of file stop the loop
+            if not line:
+                break
             # if line starts with # or is blank -> move on
             # else
+            if line[0] != '#' and line[0] != '\n':
                 #get name
+                name, junk = line.split(':')
+                line = ins.readline()
                 #get buy price
+                junk, buy = line.split(': ')
+                line = ins.readline()
                 #get sell price
+                junk, sell = line.split(': ') 
+                line = ins.reatline()
                 #get quantity
+                junk, qty = line.split(': ')
                 #calculate individual prices
-        pass
+                buy_price = round(buy / qty, 2)
+                sell_price = round(sell / qty, 2)
+                #write to dictionary
+                self.sell_prices[name] = sell_price
+                self.buy_prices[name] = buy_price
+        ins.close()
 
     #write sale prices
     def write_sell_prices(self):
-        # make dictionary of item names and sell prices
-        # write dictionary
-        pass
+        with open(sell_file, 'w') as out:
+            for key in self.sell_prices:
+                s = key + ': ' + self.sell_prices[key]
+                out.write(s)
+        out.close()
 
     #write buy prices
     def write_buy_prices(self):
