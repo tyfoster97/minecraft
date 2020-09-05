ba87bde11c6c9028c9d7565eaaba11695e17fdea
tyfoster97 <tyfoster@outlook.com>
2020-09-04 22:50:22 -0700

    Implement error handling

--- priceparser.py
+++ priceparser.py
@@ -1,66 +1,146 @@
 #imports
 
-in_file = "data/prices.yml"
-obj_file = "data/sellable.txt"
+price_file = "data/prices.yml"
+#item_file = "data/sellable.txt"
 sell_file = "worth.yml"
-buy_file = "server-price.yml"
+buy_file = "server-prices.yml"
+
+# Tracks a Minecraft item and prices for the item
+class Item:
+    #Constructor
+    def __init__(self, name, sell=0, qty=1):
+        self.f = False #edited boolean flag
+        self.n = name
+        self.sp = sell / qty #user sell price per 1
+        self.q = qty
+    # gets name for worth.yml
+    # None -> str
+    def name(self):
+        return self.n.replace('_') #plugin compatibility
+    def getbp(self):
+        return round(self.sp * 0.4, 2)
+    def getf(self):
+        return self.f
+    # gets name for prices.yml
+    # None -> str
+    def getn(self):
+        return self.n
+    def getsp(self):
+        return round(self.sp, 2)
+    def print(self):
+        print(self.n)
+        print('  sp: ' + str(self.sp))
+        print('  bp: ' + str(self.getbp()))
+    def setsp(self, price, qty=1):
+        self.sp = price / qty
+        self.bp = price * 0.4 / qty
+        self.qty = qty
+        self.f = True #flag as edited
+
 
 #class
 class PriceParser:
 
     #constructor
     def __init__(self, file):
-        self.file = file
-        self.sell_prices = dict()
-        self.buy_prices = dict()
+        self.f = file
+        self.e = False # flag for if db is edited
+        self.idb = dict() # hash table of items
+
+    def interact(self):
+        cmd = '/'
+        while cmd != 'exit':
+            cmd = input('/')
+            c = cmd.split(' ')
+            if c[0] == 'sp':
+                n = c[1]
+                sp = float(c[2])
+                if c[1] in self.idb:
+                    self.idb[n].setsp(sp)
+                    self.e = True
+                else:
+                    print('Invalid key')
+            elif c[0] == 'get':
+                if c[1] in self.idb:
+                    i = self.idb[c[1]]
+                    i.print()
+                else:
+                    print(c[1] + ' not found')
+            elif c[0] == 'add':
+                i = Item(c[1])
+                i.setsp(float(c[2]))
+                self.e = True
+                self.idb[c[1]] = i
+            elif c[0] == 'del':
+                if input('del ' + c[1] + ' y/n?') == 'y':
+                    del self.idb[c[1]]
+            elif c[0] == 'cal':
+                p = 0
+                i = 1
+                while i < len(c):
+                    if c[i] in self.idb:
+                        p += self.idb[c[i]].getsp() * float(c[i+1])
+                        i += 2
+                    else:
+                        print('  no info for: ' + c[i])
+                        break
+                print('price: '+ str(p))
+            elif c[0] == 'save':
+                self.write_prices()
+                self.write_worth()
 
     #read input from txt file
     def read(self):
         # open file
-        ins = open(self.file, 'r')
+        items = open(self.f, 'r')
         # loop through lines
         while True:
-            line = ins.readline()
+            item = items.readline()
             # if at end of file stop the loop
-            if not line:
+            if not item:
                 break
             # if line starts with # or is blank -> move on
             # else
-            if line[0] != '#' and line[0] != '\n':
+            if item[0] != '#' and item[0] != '\n':
                 #get name
-                name, junk = line.split(':')
-                line = ins.readline().strip()
+                n, junk = item.split(':')
+                item = items.readline().strip()
                 #get buy price
-                junk, buy = line.split(': ')
-                line = ins.readline().strip()
-                #get sell price
-                junk, sell = line.split(': ') 
-                line = ins.readline().strip()
+                junk, buy = item.split(': ')
+                item = items.readline().strip()
                 #get quantity
-                junk, qty = line.split(': ')
+                junk, qty = item.split(': ')
                 #calculate individual prices
-                buy_price = round(float(buy.replace(',','')) / float(qty), 2)
-                sell_price = round(float(sell.replace(',','')) / float(qty), 2)
+                sp = float(buy.replace(',',''))
                 #write to dictionary
-                name = name.replace('_','')
-                self.sell_prices[name] = sell_price
-                self.buy_prices[name] = buy_price
-        ins.close()
+                self.idb[n] = Item(n, sp, float(qty))
+        items.close()
 
+    def write_prices(self):
+        if self.e:
+            with open(self.f, 'w') as items:
+                for name in self.idb:
+                    items.write(name + ':\n')
+                    i = self.idb[name]
+                    items.write('    buyPrice: ' + str(i.getsp()) + '\n')
+                    items.write('    quantity: 1.0\n')
+            items.close()
+        return True
+    
     #write sale prices
-    def write_sell_prices(self):
-        with open(sell_file, 'w') as out:
-            out.write('worth: \n')
-            for key in self.sell_prices:
-                s = '    ' + key + ': ' + str(self.sell_prices[key]) + '\n'
-                out.write(s)
-        out.close()
-
-    #write buy prices
-    def write_buy_prices(self):
-        pass
+    def write_worth(self):
+        if self.e:
+            with open(sell_file, 'w') as out:
+                out.write('worth: \n')
+                for key in self.idb:
+                    s = '    ' + key + ': ' + str(self.idb[key].getbp()) + '\n'
+                    out.write(s)
+            out.close()
+        return True
 
 if __name__=='__main__':
-    pp = PriceParser(in_file)
+    pp = PriceParser(price_file)
     pp.read()
-    pp.write_sell_prices()
+    pp.interact() # use database
+    pp.write_prices() # save on exit
+    pp.write_worth()
